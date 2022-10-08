d1d0075aa165ed25b4d3ab3029927286ea953ca8
Sergei <drobilenko94@mail.ru>
2022-10-08 14:11:13 +0300

    add func showModal in modals.js

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -31,9 +31,17 @@ const modals = () => {
             }
         });
     }
+
+    function showModalByTime(selector, time) {
+        setTimeout(() => {
+            document.querySelector(selector).style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        }, time);
+    }
     
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
+    showModalByTime('.popup', 60000);
 };
 
 export default modals;
