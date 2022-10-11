ca4f12664b27273bbbf660f6c2cb95fd519b194c
Sergei <drobilenko94@mail.ru>
2022-10-11 18:19:49 +0300

    added sending data to the server

--- src/js/main.js
+++ src/js/main.js
@@ -8,14 +8,12 @@ window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     // Объект с данными для отправки
-    let modalState = {
-
-    };
+    let modalState = {};
 
     changeModalState(modalState);
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
-    forms();
+    forms(modalState);
 });
